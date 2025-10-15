91bd4b05a82653bcfc0373794ae338d9cd079015
vitor <vitorhugot37@gmial.com>
2025-10-15 16:17:58 -0300

    desafion1

--- mysite/usuarios/admin.py
+++ mysite/usuarios/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Usuario
 
 # Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ("username", "nome", "email")
+    list_display_links = ("username", "nome")
+    list_filter = ("nomes")
+    search_fields = ("emai". "username")
 admin.site.register(Usuario)
